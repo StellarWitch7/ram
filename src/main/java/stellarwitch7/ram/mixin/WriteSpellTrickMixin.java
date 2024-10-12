706da0cdf2bbad998e7a567e445876780e570a58
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-10-12 15:49:13 -0400

    :3

---
+++ src/main/java/stellarwitch7/ram/mixin/WriteSpellTrickMixin.java
@@ -0,0 +1,9 @@
+package stellarwitch7.ram.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import dev.enjarai.trickster.spell.trick.basic.WriteSpellTrick;
+
+@Mixin(WriteSpellTrick.class)
+public abstract class WriteSpellTrickMixin {
+
+}
